70e881e4fdfcbcbe270d576fac5ed10362603986
“whoiskatrin” <katjareznikova@gmail.com>
2023-06-13 18:26:01 +0200

    added UI part, flask engine, paper retrieval

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # arxivy
-Something around arxiv and LLM
+
+# ArxivY
+
+A simple web app to fetch and display latest papers from ArXiv.
+
+## Installation
+
+1. Clone the repository:
+
+   ```
+   git clone https://github.com/tnachen/arxivy.git
+   ```
+
+2. Install the dependencies:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+3. Run the application:
+
+   ```
+   python run.py
+   ```
+
+The application should now be running at http://localhost:5000.
+
+## Usage
+
+Open
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template
+from app import app
+import requests
+from bs4 import BeautifulSoup
+
+@app.route('/')
+def index():
+    category = 'cs.AI'  # to choose a different category
+    xml_data = fetch_arxiv_papers(category)
+    papers = parse_arxiv_papers(xml_data)
+    return render_template('index.html', papers=papers)
+
+def fetch_arxiv_papers(category):
+    url = f'http://export.arxiv.org/api/query?search_query=cat:{category}&sortBy=submittedDate&sortOrder=descending'
+    response = requests.get(url)
+    return response.content
+
+def parse_arxiv_papers(xml_data):
+    soup = BeautifulSoup(xml_data, 'xml')
+    papers = []
+    for entry in soup.find_all('entry'):
+        paper = {
+            'title': entry.title.text,
+            'summary': entry.summary.text,
+            'authors': [author.text for author in entry.find_all('name')],
+            'published': entry.published.text,
+            'link': entry.id.text,
+        }
+        papers.append(paper)
+    return papers
