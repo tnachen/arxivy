c9c8f809b1c3824012246f0e110e511cf1248040
“whoiskatrin” <katjareznikova@gmail.com>
2023-08-25 16:15:23 +0200

    small design tweaks

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask
+from flask_cors import CORS
+
+# Initialize CORS
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.secret_key = os.urandom(24)
 from app import routes
